fb7a1e1af5ca8c6dc0840229192c8f7a7345a22b
google.account@anumbersgame.net <google.account@anumbersgame.net@7edbea1e-33a7-11de-be05-5f7a86268029>
2009-04-29 07:13:08 +0000

    Create databases, add a new table, script it all.

---
+++ trunk/data/debateresults/converted/mysql/import.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mysql DebateResults0304 < 0304.mysql
+mysql DebateResults0304 < ballots.mysql
+
+mysql DebateResults0405 < 0405.mysql
+mysql DebateResults0405 < ballots.mysql
+
+mysql DebateResults0506 < 0506.mysql
+mysql DebateResults0506 < ballots.mysql
+
+mysql DebateResults0607 < 0607.mysql
+mysql DebateResults0607 < ballots.mysql
+
+mysql DebateResults0708 < 0708.mysql
+mysql DebateResults0708 < ballots.mysql
