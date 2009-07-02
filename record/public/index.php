800ba8cfc5774dc56808f006ef793e436f526cd5
google.account <google.account@anumbersgame.net>
2009-07-02 17:23:46 +0000

    Use helper files

--- record/public/index.php
+++ record/public/index.php
@@ -1,15 +1,14 @@
 <?php
 
-// put full path to Smarty.class.php
-require('../protected/Smartylib/Smarty.class.php');
-$smarty = new Smarty();
 
-$smarty->template_dir = '../protected/smarty/templates';
-$smarty->compile_dir = '../protected/smarty/templates_c';
-$smarty->cache_dir = '../protected/smarty/cache';
-$smarty->config_dir = '../protected/smarty/configs';
+require_once ('../protected/smarty/template.php');
+$template = new template();
 
-$smarty->assign('name', 'World');
-$smarty->display('index.tpl');
+require_once ('../protected/db/dbview.php');
+
+$dbh = new dbview();
+
+$template->assign('name', 'World');
+$template->display('index.tpl');
 
 ?>
