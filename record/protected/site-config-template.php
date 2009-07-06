3c902c7548f4db939b5d0deaf78e9778f6303c9c
google.account <google.account@anumbersgame.net>
2009-07-06 23:15:18 +0000

    new OO config template, forgot to copy from site-config.php proper

--- record/protected/site-config-template.php
+++ record/protected/site-config-template.php
@@ -1,7 +1,12 @@
 <?php
 
-$dbviewhost = '';
-$dbviewuser = '';
-$dbviewpass = '';
+class siteConfig 
+{
+  public static function dbviewhost() { return ''; }
+  public static function dbviewuser() { return ''; }
+  public static function dbviewpass() { return ''; }
+
+  public static function webroot() { return ''; }
+}
 
 ?>
