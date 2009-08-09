835b2f76c9ba81feb993eafcc6a219e9ad7d6f83
google.account@anumbersgame.net <google.account@anumbersgame.net@7edbea1e-33a7-11de-be05-5f7a86268029>
2009-08-09 21:09:05 +0000

    simple fair bets calculation

---
+++ trunk/tests/fair-bets/eig.ml
@@ -0,0 +1,39 @@
+(*
+
+Requires lacaml 5
+
+*)
+
+open Bigarray
+
+open Lacaml.Impl.D
+
+(*
+*  WR      (output) DOUBLE PRECISION array, dimension (N)
+*  WI      (output) DOUBLE PRECISION array, dimension (N)
+*          WR and WI contain the real and imaginary parts,
+*          respectively, of the computed eigenvalues.  Complex
+*          conjugate pairs of eigenvalues appear consecutively
+*          with the eigenvalue having the positive imaginary part
+*          first.
+*  VR      (output) DOUBLE PRECISION array, dimension (LDVR,N)
+*          If JOBVR = 'V', the right eigenvectors v(j) are stored one
+*          after another in the columns of VR, in the same order
+*          as their eigenvalues.
+*          If JOBVR = 'N', VR is not referenced.
+*          If the j-th eigenvalue is real, then v(j) = VR(:,j),
+*          the j-th column of VR.
+*          If the j-th and (j+1)-st eigenvalues form a complex
+*          conjugate pair, then v(j) = VR(:,j) + i*VR(:,j+1) and
+*          v(j+1) = VR(:,j) - i*VR(:,j+1).
+ *)
+
+(*
+http://www.netlib.org/lapack/double/dgeev.f
+examples/eig/eig.ml in lacaml
+*)
+
+let rightEigenColumns wins =
+  let winmat = Mat.of_array wins in
+  let _, wr, wi, right = geev (lacpy winmat) in
+    wr, wi, right
