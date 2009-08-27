976107b77593c6e075fcf9d44b5a73fa129e6019
google.account <google.account@anumbersgame.net>
2009-08-27 07:31:31 +0000

    Make a better table

--- tests/judge-side-bias/hanes.sql
+++ tests/judge-side-bias/hanes.sql
@@ -16,19 +16,34 @@ where many >= 6;
 
 create temporary table probs
 select year
-,count(if(decision = 'aff',1,null))/
+,count(if(decision = 'aff',1,null)) /
  count(if(decision in ('aff','neg'),1,null)) as prob
 from ballots
 group by year;
 
 create temporary table color
 select must.year,
-affcount < (tcount * prob) - sqrt(tcount * prob * (1-prob))
-or
-affcount > (tcount * prob) + sqrt(tcount * prob * (1-prob)) as nongreen,
+affcount < (tcount * prob) + sqrt(tcount * prob * (1-prob))
+and
+affcount > (tcount * prob) - sqrt(tcount * prob * (1-prob)) as ingreen,
 affcount = (tcount * prob) - sqrt(tcount * prob * (1-prob))
 or
-affcount = (tcount * prob) + sqrt(tcount * prob * (1-prob)) as ongreen
+affcount = (tcount * prob) + sqrt(tcount * prob * (1-prob)) as ongreen,
+affcount < (tcount * prob) + 2*sqrt(tcount * prob * (1-prob))
+and
+affcount > (tcount * prob) - 2*sqrt(tcount * prob * (1-prob)) as inyellow,
+affcount = (tcount * prob) - 2*sqrt(tcount * prob * (1-prob))
+or
+affcount = (tcount * prob) + 2*sqrt(tcount * prob * (1-prob)) as onyellow,
+affcount < (tcount * prob) + 3*sqrt(tcount * prob * (1-prob))
+and
+affcount > (tcount * prob) - 3*sqrt(tcount * prob * (1-prob)) as inred,
+affcount = (tcount * prob) - 3*sqrt(tcount * prob * (1-prob))
+or
+affcount = (tcount * prob) + 3*sqrt(tcount * prob * (1-prob)) as onred,
+affcount > (tcount * prob) + 3*sqrt(tcount * prob * (1-prob))
+or
+affcount < (tcount * prob) - 3*sqrt(tcount * prob * (1-prob)) as outred
 from (select people.year, people.aka,
 count(if(ballots.decision = 'aff',1,null)) as affcount,
 count(if(ballots.decision in ('aff','neg'),1,null)) as tcount
@@ -41,6 +56,11 @@ and people.id = ballots.judge
 group by year, people.aka) as must, probs
 where must.year = probs.year;
 
-select year, avg(nongreen), avg(ongreen)
+select concat('||',year, 
+concat('||',round(100*avg(ingreen),2),'%'),
+concat('||',round(100*avg(inyellow),2),'%'),
+concat('||',round(100*avg(inred),2),'%'),
+concat('||',round(100*avg(outred),2),'%'),
+'||')
 from color
 group by year;
